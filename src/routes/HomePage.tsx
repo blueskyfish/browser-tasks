67befe815a5dd0f5455c156fb1af39fad7b4ea03
Sarah Kirchner <sarah.kirchner@sulzer.de>
2023-09-28 15:10:39 +0200

    Card in small devices

--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -1,10 +1,12 @@
 import { mdiHomeOutline } from '@mdi/js';
 import { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import CardTaskList from '../components/CardTaskList';
 import DataTaskList from '../components/DataTaskList';
 import Header from '../components/Header';
 import SidebarContext from '../context/sidebar.context';
 import { ResponsiveContext } from '../reponsive/ResponsiveContext';
+import { ResponsiveSize } from '../reponsive/ResponsiveModel';
 import DataContext from '../store/DataContext';
 import { Task } from '../store/TaskModel';
 import './HomePage.css';
@@ -25,10 +27,13 @@ export default function HomePage() {
         navigate(`/task/${task.id}`);
     };
 
+    const isSmallSize = size === ResponsiveSize.xs || size === ResponsiveSize.md;
+
     return (
         <>
             <Header title={"Home"} icon={mdiHomeOutline}/>
-            <DataTaskList taskList={taskList} size={size} onTask={handleTask}/>
+            {isSmallSize && (<DataTaskList taskList={taskList} size={size} onTask={handleTask}/>)}
+            {!isSmallSize && (<CardTaskList taskList={taskList} onTask={handleTask}/>)}
         </>
 
     );
