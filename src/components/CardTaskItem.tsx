9b0c80abdb91dcc55d64d651c1c6310589d68938
Sarah Kirchner <sarah.kirchner@sulzer.de>
2023-09-28 15:11:07 +0200

    Create CardTaskItem

---
+++ src/components/CardTaskItem.tsx
@@ -0,0 +1,33 @@
+import { mdiFileEditOutline } from '@mdi/js';
+import Icon from '@mdi/react';
+import { Avatar, Card, CardContent, CardHeader, IconButton } from '@mui/material';
+import { Task } from '../store/TaskModel';
+import ContentBox from './ContentBox';
+import { getTaskStatusIcon } from './TaskStatusIcon';
+
+export type CardTaskItemProps = {
+    task: Task;
+    onTask: (task: Task) => void;
+}
+
+export default function CardTaskItem({task, onTask}: CardTaskItemProps) {
+    return (
+        <Card sx={{marginBottom: 'var(--gap-2)'}}>
+            <CardHeader
+                avatar={(
+                    <Icon path={getTaskStatusIcon(task.status)} size={2}/>
+                )}
+                title={task.title}
+                subheader={task.dueDate ?? '-'}
+                action={(
+                    <IconButton onClick={() => onTask(task)}>
+                        <Icon path={mdiFileEditOutline} size={1}/>
+                    </IconButton>
+                )}
+            />
+            <CardContent>
+                <ContentBox label="Task Description" content={task.content}/>
+            </CardContent>
+        </Card>
+    );
+}
