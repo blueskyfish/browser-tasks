15abc3621970bbca4ea3b02f02123ada6bdbbc5f
Sarah Kirchner <sarah.kirchner@sulzer.de>
2023-09-28 15:10:39 +0200

    Card in small devices

---
+++ src/components/CardTaskList.tsx
@@ -0,0 +1,17 @@
+import { Box } from '@mui/material';
+import { Task } from '../store/TaskModel';
+import CardTaskItem from './CardTaskItem';
+
+export type CardTaskListProps = {
+    taskList: Task[];
+    onTask: (task: Task) => void;
+}
+export default function CardTaskList({taskList, onTask}: CardTaskListProps) {
+    return (
+        <Box sx={{ padding: 'var(--gap-1)'}}>
+            {taskList.map((task: Task, index: number) => (
+                <CardTaskItem key={index} task={task} onTask={onTask}/>
+            ))}
+        </Box>
+    )
+}
